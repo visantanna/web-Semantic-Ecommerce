a6e144468d8d2a63ea57603deca89c68c0518e4e
Vinícius <3975749@gmail.com>
2019-12-10 03:45:03 -0300

    linkando lojas com produtos , através do id da loja

--- src/main/java/com/example/demo/business/ProductsBusiness.java
+++ src/main/java/com/example/demo/business/ProductsBusiness.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,7 @@ import com.example.demo.dto.Product;
 
 @Service
 public class ProductsBusiness {
-	Product tenisNike = new Product("593c1e3e-1a53-11ea-978f-2e728ce88125",177.90, "Nike SB50" , "c3f6f6ae-1a44-11ea-978f-2e728ce88125", "Nike","https://c.static-nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/uawxvnh1undj94govvic/sb50-nike-free-trainer-30-v4-metallic-gold.jpg" );
+	Product tenisNike = new Product("593c1e3e-1a5311ea978f2e728ce88125",177.90, "Nike SB50" , "c3f6f6ae1a4411ea978f2e728ce88125", "Nike","https://c.static-nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/uawxvnh1undj94govvic/sb50-nike-free-trainer-30-v4-metallic-gold.jpg" );
 	ArrayList<Product> listProducts = new ArrayList<Product>(Arrays.asList(tenisNike));
 	
 	public Product createNewProduct(Product product) {
@@ -21,7 +22,8 @@ public class ProductsBusiness {
 		return product;
 	}
 	public List<Product> listProductsFromStore(String id){
-		return listProducts;
+		List<Product> responseList = (ArrayList<Product>) listProducts.stream().filter(prod-> prod.getIdStore().equals(id)).collect(Collectors.toList());
+		return responseList;
 	}
 
 }
