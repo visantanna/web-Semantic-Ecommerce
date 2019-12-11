849f1c854714868950de072882f6a300a0981b9a
ericamk <erimk@live.com>
2019-12-11 14:30:24 -0200

    owl e dependencias

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		loadOwl();
 		SpringApplication.run(DemoApplication.class, args);
 		
+	}
+	
+	private static void loadOwl() {
+		String path = "src/main/resources/EP_WS_RDFXML.owl" ;
+		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+		model.read(path);
+		model.write(System.out);
 	}
 
 }
